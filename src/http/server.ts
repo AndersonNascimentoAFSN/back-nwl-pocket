f9e8d3b80a651139fa316682008a9c35b04c0b1d
Anderson Nascimento <anderson.nascimento@meta.com.br>
2024-09-12 09:11:14 -0300

    feat: add use cases create goal completion and get week pendings

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,48 +1,95 @@
 import fastify from 'fastify'
 import z from 'zod'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider
+} from "fastify-type-provider-zod";
 
 import { env } from '../env'
 import { createGoal } from '../use-cases/create-goal'
 import { fetchGoals } from '../use-cases/fetch-goals'
+import { getWeekPendingGoals } from '../use-cases/get-week-pending-goals';
+import { createGoalCompletion } from '../use-cases/create-goal-completion';
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.post('/goals', async (request, reply) => {
-  const createGoalSchema = z.object({
-    title: z.string(),
-    desiredWeeklyFrequency: z.number(),
-  })
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-  const body = createGoalSchema.safeParse(request.body)
 
-  if (body.error) {
-    reply.send(body.error.errors).code(400)
-    return
-  }
+app.get('/pending-goals', async (request, reply) => {
+  const pendingGoals = await getWeekPendingGoals()
 
-  const goal = await createGoal({
-    title: body.data.title,
-    desiredWeeklyFrequency: body.data.desiredWeeklyFrequency,
-  })
-
-  reply.send(goal).code(201)
+  reply.send(pendingGoals).code(200)
 })
 
-app.get('/goals', async (request, reply) => {
-  const query = z.object({
-    page: z.coerce.number().default(1),
-    limit: z.coerce.number().default(10),
-  }).safeParse(request.query)
+app.post('/goal-completions',
+  {
+    schema: {
+      body: z.object({
+        goalId: z.string(),
+      }),
+    },
+  }, async (request, reply) => {
+    const { goalId } = request.body
+    console.log('goalId', goalId)
 
-  if (query.error) {
-    reply.send(query.error.errors).code(400)
-    return
+    const goal = await createGoalCompletion({
+      goalId,
+    })
+
+    reply.send(goal).code(201)
+  })
+
+app.post('/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z
+          .number()
+          .int()
+          .min(1)
+          .max(7),
+      }),
+    },
+  }, async (request, reply) => {
+    const { title, desiredWeeklyFrequency } = request.body
+
+    const goal = await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+
+    reply.send(goal).code(201)
+  })
+
+app.get('/goals',
+  {
+    schema: {
+      querystring: z.object({
+        page: z.coerce.number().default(1),
+        limit: z.coerce.number().default(10),
+      }),
+    }
   }
+  , async (request, reply) => {
+    // const query = z.object({
+    //   page: z.coerce.number().default(1),
+    //   limit: z.coerce.number().default(10),
+    // }).safeParse(request.query)
 
-  const goal = await fetchGoals(query.data)
+    // if (query.error) {
+    //   reply.send(query.error.errors).code(400)
+    //   return
+    // }
+    const { query } = request
 
-  reply.send(goal).code(200)
-})
+    const goal = await fetchGoals(query)
+
+    reply.send(goal).code(200)
+  })
 
 app.listen({
   port: env.PORT,
